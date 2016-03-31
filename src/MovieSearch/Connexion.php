e8cf5f3f9a211a943ca7a08b60abcdf1327bd22f
Michael Guerfi <michaelguerfi@gmail.com>
2016-03-31 21:57:12 +0200

    Ajout requetes

--- src/MovieSearch/Connexion.php
+++ src/MovieSearch/Connexion.php
@@ -1,25 +1,20 @@
 <?php
 namespace MovieSearch;
-
 class Connexion{
-
   private static $_instance = null;
   private $conn;
-
   private function __construct(){
     $config = new \Doctrine\DBAL\Configuration();
     $connectionParams = array(
-      'url' => 'mysql://root:root@127.0.0.1/moviesearch?charset=utf8mb4',
-
+        'url' => 'mysql://root:root@127.0.0.1/moviesearch?charset=utf8mb4',
     );
     $this->conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
   }
-
   public static function getInstance(){
     if(is_null(self::$_instance) ){
-      self::$_instance = new Connexion();  
+      self::$_instance = new Connexion();
     }
     return self::$_instance->conn;
   }
-
 }
+
